aacbac19995b6625864cb4359f74ebfce279264a
Arindam <i.am.ari.chakraborty@gmail.com>
2023-03-10 20:13:05 +0530

    added post request

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ app.get("/foo", function (req, res) {
 	res.send("Hello, It's Working and you're at foo");
 });
 
-app.post("/bar", (req, res) => {
-	var body = req.body;
-	console.log(body.foo);
-	res.send(req.body.foo);
-});
-
 app.use(
 	express.urlencoded({
 		extended: true,
 	})
 );
+
+app.post("/bar", (req, res) => {
+	var body = req.body;
+	console.log(body.foo);
+	res.send(req.body.foo);
+});
